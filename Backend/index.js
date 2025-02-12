5210cd9814346311df6d78d6e74a0625f9419b6f
illum1nadi <adityainduri@gmail.com>
2025-02-12 15:48:36 +0530

    Added logic in express.

--- Backend/index.js
+++ Backend/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const {createTodo, updateTodo} = require("./types");
 const app = express();
 
 app.use(express.json());
@@ -16,14 +17,29 @@ app.use(express.json());
   }
 */
 
-app.post("/todo", (req, res) = {
+app.post("/todo", (req, res) => {
+  const createPayload = req.body;
+  const parsedPayload = createTodo.safeParse(createPayload);
+  if(!parsedPayload.success) {
+    res.status(411).json({
+      msg : "You sent the wrong inputs."
+    });
+    return;
+  }
 
 })
 
-app.get("/todo", (req, res) = {
+app.get("/todos", (req, res) = {
   
 })
 
-app.put("/completed", (req, res) = {
-  
+app.put("/completed", (req, res) => {
+  const createPayload = req.body;
+  const parsedPayload = updateTodo.safeParse(createPayload);
+  if(!parsedPayload.success) {
+    res.status(411).json({
+      msg : "You sent the wrong inputs."
+    });
+    return;
+  }
 })

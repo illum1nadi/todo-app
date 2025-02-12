d4ede3b80dc4e09443b20ce4efd9944f93947292
illum1nadi <adityainduri@gmail.com>
2025-02-12 13:23:37 +0530

    Added backend folder and express with its boilerplate.

---
+++ Backend/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.post("/todo", (req, res) = {
+
+})
+
+app.get("/todo", (req, res) = {
+  
+})
+
+app.put("/todo", (req, res) = {
+  
+})
